551d5a64e14e384aef108c09a44dadffb2a5cc01
Kevin Calle <kcalle012@gmail.com>
2024-02-07 18:36:25 -0500

    added bottom sidebar buttons

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,16 @@ import './index.scss';
 import Sidebar from '../Sidebar'
 
 const Layout = () => {
-    return <><Sidebar /></>
+    return (
+    <div className='App'>
+        <Sidebar />
+        <div className='page'>
+            <span className='tag top-tags'>&lt;body&gt;</span>
+
+        </div>
+    </div>
+    
+    )
 }
 
 export default Layout
